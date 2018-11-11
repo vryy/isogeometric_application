9691f680453a0b39cbde85c65443ccfd46765fe1
vryy <hgbk2008@gmail.com>
2018-11-11 23:49:59 +0100

    introduce abstract class Cell

--- custom_utilities/nurbs/bcell.h
+++ custom_utilities/nurbs/bcell.h
@@ -6,8 +6,8 @@
 //
 //
 
-#if !defined(KRATOS_ISOGEOMETRIC_APPLICATION_CELL_H_INCLUDED )
-#define  KRATOS_ISOGEOMETRIC_APPLICATION_CELL_H_INCLUDED
+#if !defined(KRATOS_ISOGEOMETRIC_APPLICATION_BCELL_H_INCLUDED )
+#define  KRATOS_ISOGEOMETRIC_APPLICATION_BCELL_H_INCLUDED
 
 // System includes
 #include <string>
@@ -19,6 +19,7 @@
 
 // Project includes
 #include "includes/define.h"
+#include "custom_utilities/cell.h"
 #include "custom_utilities/nurbs/knot.h"
 
 namespace Kratos
@@ -37,38 +38,36 @@ struct BCell_Helper
  * A cell is the smaller unit in the isogeometric topology mesh (e.g. it represents an element, or Bezier element of the T-splines basis function).
  * A cell is determined by its topology index of its vertices.
  */
-class BCell
+class BCell : public Cell
 {
 public:
     /// Pointer definition
     KRATOS_CLASS_POINTER_DEFINITION(BCell);
 
     /// Type definitions
+    typedef Cell BaseType;
     typedef Knot<double> KnotType;
     typedef KnotType::Pointer knot_t;
-    typedef boost::numeric::ublas::mapped_vector<double> SparseVectorType;
+    typedef BaseType::SparseVectorType SparseVectorType;
     // typedef boost::numeric::ublas::vector<double> SparseVectorType;
 
     /// Constructor with knots
     BCell(const std::size_t& Id, knot_t pXiMin, knot_t pXiMax)
-    : mId(Id), mpXiMin(pXiMin), mpXiMax(pXiMax), mpEtaMax(new KnotType(0.0)), mpEtaMin(new KnotType(0.0)), mpZetaMax(new KnotType(0.0)), mpZetaMin(new KnotType(0.0))
+    : BaseType(Id), mpXiMin(pXiMin), mpXiMax(pXiMax), mpEtaMax(new KnotType(0.0)), mpEtaMin(new KnotType(0.0)), mpZetaMax(new KnotType(0.0)), mpZetaMin(new KnotType(0.0))
     {}
 
     /// Constructor with knots
     BCell(const std::size_t& Id, knot_t pXiMin, knot_t pXiMax, knot_t pEtaMin, knot_t pEtaMax)
-    : mId(Id), mpXiMin(pXiMin), mpXiMax(pXiMax), mpEtaMax(pEtaMax), mpEtaMin(pEtaMin), mpZetaMax(new KnotType(0.0)), mpZetaMin(new KnotType(0.0))
+    : BaseType(Id), mpXiMin(pXiMin), mpXiMax(pXiMax), mpEtaMax(pEtaMax), mpEtaMin(pEtaMin), mpZetaMax(new KnotType(0.0)), mpZetaMin(new KnotType(0.0))
     {}
 
     /// Constructor with knots
     BCell(const std::size_t& Id, knot_t pXiMin, knot_t pXiMax, knot_t pEtaMin, knot_t pEtaMax, knot_t pZetaMin, knot_t pZetaMax)
-    : mId(Id), mpXiMin(pXiMin), mpXiMax(pXiMax), mpEtaMax(pEtaMax), mpEtaMin(pEtaMin), mpZetaMax(pZetaMax), mpZetaMin(pZetaMin)
+    : BaseType(Id), mpXiMin(pXiMin), mpXiMax(pXiMax), mpEtaMax(pEtaMax), mpEtaMin(pEtaMin), mpZetaMax(pZetaMax), mpZetaMin(pZetaMin)
     {}
 
     /// Destructor
     virtual ~BCell() {}
-
-    /// Get the Id
-    std::size_t Id() const {return mId;}
 
     /// Get the coordinates
     knot_t XiMin() const {return mpXiMin;}
@@ -192,118 +191,6 @@ public:
         return false;
     }
 
-    /// Clear internal data of this cell
-    void Reset()
-    {
-        mSupportedAnchors.clear();
-        mAnchorWeights.clear();
-        mCrows.clear();
-    }
-
-    /// Add supported anchor and the respective extraction operator of this cell to the anchor
-    void AddAnchor(const std::size_t& Id, const double& W, const Vector& Crow)
-    {
-        mSupportedAnchors.push_back(Id);
-        mAnchorWeights.push_back(W);
-
-        std::vector<std::size_t> inz;
-        inz.reserve(Crow.size());
-        for (std::size_t i = 0; i < Crow.size(); ++i)
-            if (Crow[i] != 0.0)
-                inz.push_back(i);
-        SparseVectorType Crow_sparse(Crow.size(), inz.size());
-        for (std::size_t i = 0; i < inz.size(); ++i)
-            Crow_sparse[inz[i]] = Crow[inz[i]];
-        mCrows.push_back(Crow_sparse);
-    }
-
-    /// Absorb the information from the other cell
-    virtual void Absorb(BCell::Pointer pOther)
-    {
-        for (std::size_t i = 0; i < pOther->NumberOfAnchors(); ++i)
-        {
-            if (std::find(mSupportedAnchors.begin(), mSupportedAnchors.end(), pOther->GetSupportedAnchors()[i]) == mSupportedAnchors.end())
-            {
-                this->AddAnchor(pOther->GetSupportedAnchors()[i], pOther->GetAnchorWeights()[i], pOther->GetCrows()[i]);
-            }
-        }
-    }
-
-    /// This action is called when the cell is removed from the cell manager, see e.g. hb_cell.
-    virtual void ClearTrace()
-    {
-        // DO NOTHING
-    }
-
-    /// Get the number of supported anchors of this cell. In other words, it is the number of basis functions that the support domain includes this cell.
-    std::size_t NumberOfAnchors() const {return mSupportedAnchors.size();}
-
-    /// Get the supported anchors of this cell
-    const std::vector<std::size_t>& GetSupportedAnchors() const {return mSupportedAnchors;}
-
-    /// Get the weights of all the supported anchors
-    const std::vector<double>& GetAnchorWeights() const {return mAnchorWeights;}
-    void GetAnchorWeights(Vector& rWeights) const
-    {
-        if(rWeights.size() != mAnchorWeights.size())
-            rWeights.resize(mAnchorWeights.size(), false);
-        std::copy(mAnchorWeights.begin(), mAnchorWeights.end(), rWeights.begin());
-    }
-
-    /// Get the internal data of row of the extraction operator
-    const std::vector<SparseVectorType>& GetCrows() const {return mCrows;}
-
-    /// Get the extraction operator matrix
-    Matrix GetExtractionOperator() const
-    {
-        Matrix M(mCrows.size(), mCrows[0].size());
-        for(std::size_t i = 0; i < mCrows.size(); ++i)
-            noalias(row(M, i)) = mCrows[i];
-        return M;
-    }
-
-    /// Get the extraction as compressed matrix
-    CompressedMatrix GetCompressedExtractionOperator() const
-    {
-        CompressedMatrix M(mCrows.size(), mCrows[0].size());
-        // here we just naively copy the data. However we shall initialize the compressed matrix properly as in the kernel (TODO)
-        for(std::size_t i = 0; i < mCrows.size(); ++i)
-            noalias(row(M, i)) = mCrows[i];
-        M.complete_index1_data();
-        return M;
-    }
-
-    /// Get the extraction operator as CSR triplet
-    void GetExtractionOperator(std::vector<int>& rowPtr, std::vector<int>& colInd, std::vector<double>& values) const
-    {
-        int cnt = 0;
-        rowPtr.push_back(cnt);
-        for(std::size_t i = 0; i < mCrows.size(); ++i)
-        {
-            for(std::size_t j = 0; j < mCrows[i].size(); ++j)
-            {
-                if(mCrows[i](j) != 0)
-                {
-                    colInd.push_back(j);
-                    values.push_back(mCrows[i](j));
-                    ++cnt;
-                }
-            }
-            rowPtr.push_back(cnt);
-        }
-    }
-
-    /// Implement relational operator for automatic arrangement in container
-    inline bool operator==(const BCell& rA) const
-    {
-        return this->Id() == rA.Id();
-    }
-
-    inline bool operator<(const BCell& rA) const
-    {
-        return this->Id() < rA.Id();
-    }
-
     /// Information
     virtual void PrintInfo(std::ostream& rOStream) const
     {
@@ -322,16 +209,12 @@ public:
 
 private:
 
-    std::size_t mId;
     knot_t mpXiMin;
     knot_t mpXiMax;
     knot_t mpEtaMax;
     knot_t mpEtaMin;
     knot_t mpZetaMax;
     knot_t mpZetaMin;
-    std::vector<std::size_t> mSupportedAnchors;
-    std::vector<double> mAnchorWeights; // weight of the anchor
-    std::vector<SparseVectorType> mCrows; // bezier extraction operator row to each anchor
 };
 
 template<>
@@ -372,5 +255,5 @@ inline std::ostream& operator <<(std::ostream& rOStream, const BCell& rThis)
 
 }// namespace Kratos.
 
-#endif // KRATOS_ISOGEOMETRIC_APPLICATION_CELL_H_INCLUDED
+#endif // KRATOS_ISOGEOMETRIC_APPLICATION_BCELL_H_INCLUDED
 

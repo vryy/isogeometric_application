ec45d80efed8e514acdf2b7ac668f1fee264fa62
Hoang-Giang Bui <hgbk2008@gmail.com>
2024-12-11 21:09:55 +0100

    Preserve the layer index after refinement

--- custom_utilities/multipatch_refinement_utility.hpp
+++ custom_utilities/multipatch_refinement_utility.hpp
@@ -68,6 +68,7 @@ void MultiPatchRefinementUtility::InsertKnots(typename Patch<TDim>::Pointer& pPa
         // create new patch with same Id
         typename Patch<TDim>::Pointer pNewPatch = typename Patch<TDim>::Pointer(new Patch<TDim>(pPatch->Id()));
         pNewPatch->SetPrefix(pPatch->Prefix());
+        pNewPatch->SetLayerIndex(pPatch->LayerIndex());
 
         // compute the transformation matrix
         std::vector<std::vector<double> > new_knots(TDim);
@@ -279,6 +280,7 @@ void MultiPatchRefinementUtility::DegreeElevate(typename Patch<TDim>::Pointer& p
         // create new patch with same Id
         typename Patch<TDim>::Pointer pNewPatch = typename Patch<TDim>::Pointer(new Patch<TDim>(pPatch->Id()));
         pNewPatch->SetPrefix(pPatch->Prefix());
+        pNewPatch->SetLayerIndex(pPatch->LayerIndex());
 
         // elevate the degree and initialize new patch
         typename BSplinesFESpace<TDim>::Pointer pFESpace = iga::dynamic_pointer_cast<BSplinesFESpace<TDim> >(pPatch->pFESpace());
